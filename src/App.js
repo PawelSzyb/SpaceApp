b3a42b490224aeb1e28754c873856b8d9062f7bc
PawelSzyb <pawelszybaj@@wp.pl>
2018-07-25 11:18:50 +0200

    Create basic view app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./components/Home.js";
+import About from "./components/About.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hi</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </Router>
     );
   }
 }
