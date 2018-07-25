c7908c882fdde8091ed1728b1fa025ba194ec55a
PawelSzyb <pawelszybaj@@wp.pl>
2018-07-25 14:13:40 +0200

    search input and submit button

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className="wrapper">
         <h1>About</h1>
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import debounce from "lodash.debounce";
+
+import "./Home.css";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "",
+      apiUrl: "https://images-api.nasa.gov/search"
+    };
+    this.onSearchTermChange = this.onSearchTermChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSearchTermChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    axios
+      .get(`${this.state.apiUrl}?q=${this.state.searchTerm}&media_type=image`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <div className="wrapper">
+        <div className="search">
+          <form onSubmit={this.onSubmit}>
+            <label htmlFor="search">Search</label>
+            <input
+              id="search"
+              value={this.state.searchTerm}
+              name="searchTerm"
+              onChange={this.onSearchTermChange}
+            />
+            <button class="btn" type="btn" onClick={this.onSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
